01e6db32f0dab979eabef6007acfc3512c4b6b49
Nikotyan <lokey2719@gmail.com>
2024-08-11 14:14:24 +0300

    [develop] - комит

--- src/assets/components/LeftSide/LeftSide.jsx
+++ src/assets/components/LeftSide/LeftSide.jsx
@@ -6,30 +6,33 @@ import { Stage, Layer, Image, Text, Transformer, Circle } from 'react-konva';
 const LeftSide = () => {
   const stageRef = useRef(null);
   const imageRef = useRef(null);
+  const stickerRef = useRef(null);
   const fileInputRef = useRef(null);
+  const fileStickerRef = useRef(null);
   const textRef = useRef(null);
   const transformerRef = useRef(null);
   const [backgroundImage, setBackgroundImage] = useState(null);
   const [bearImage, setBearImage] = useState(null);
-  const [selectedText, setSelectedText] = useState(null);
+  //
   const [text, setText] = useState({ value: '', x: 0, y: 0 });
+  const [selectedText, setSelectedText] = useState(null);
   const [textScale, setTextScale] = useState({ scaleX: 1, scaleY: 1 });
   const [isTextSized, setIsTextSized] = useState(false);
   const [showDeleteButton, setShowDeleteButton] = useState(false);
+  //
+  const [stickerImage, setStickerImage] = useState(null);
+  const [selectedSticker, setSelectedSticker] = useState(null);
+  const [stickerScale, setStickerScale] = useState({ scaleX: 1, scaleY: 1 });
+  const [isStickerSized, setIsStickerSized] = useState(true);
 
-  const handleTextClick = (e) => {
-    setSelectedText(e.target);
-    setIsTextSized(false);
-    setShowDeleteButton(true);
-  };
-
-  const handleDeleteText = () => {
-    if (textRef.current) {
-      textRef.current.destroy();
-      setSelectedText(null);
-      setShowDeleteButton(false);
-      setText({ value: '', x: 0, y: 0 });
-      stageRef.current.batchDraw();
+  //
+  const [selectedType, setSelectedType] = useState(null);
+  const handleStickerChange = (event) => {
+    console.log('in sticker');
+    const file = event.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setStickerImage(imageUrl);
     }
   };
 
@@ -53,17 +56,33 @@ const LeftSide = () => {
     }
   }, [backgroundImage]);
 
+  useEffect(() => {
+    if (stickerImage) {
+      const img = new window.Image();
+      img.src = stickerImage;
+      img.onload = () => {
+        stickerRef.current.image(img);
+        stageRef.current.batchDraw();
+      };
+    }
+  }, [stickerImage]);
+
   const handleBackgroundUpload = () => {
     fileInputRef.current.click();
   };
+  const handleStickerUpload = () => {
+    fileStickerRef.current.click();
+  };
 
   const handleFileChange = (event) => {
+    console.log('in background');
     const file = event.target.files[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setBackgroundImage(imageUrl);
     }
   };
+
   const handleAddText = () => {
     const value = prompt('Enter the text:');
     if (value) {
@@ -78,18 +97,71 @@ const LeftSide = () => {
       const scaleY = node.scaleY();
 
       setTextScale({ scaleX, scaleY });
-      setIsTextSized(true);
     }
   };
+  const handleStickerTransformEnd = () => {
+    if (stickerRef.current) {
+      const node = stickerRef.current;
+      const scaleX = node.scaleX();
+      const scaleY = node.scaleY();
+      const rotation = node.rotation();
+
+      setStickerScale({ scaleX, scaleY, rotation });
+    }
+  };
+
+  const handleTextClick = (e) => {
+    console.log('выбран текст');
+    setSelectedText(e.target);
+    setIsTextSized(false);
+    setShowDeleteButton(true);
+    setSelectedType('text');
+    setSelectedSticker(null);
+  };
+  const handleStickerClick = (e) => {
+    console.log('выбран стикер');
+    setSelectedSticker(e.target);
+    setIsStickerSized(false);
+    setShowDeleteButton(true);
+    setSelectedType('sticker');
+    setSelectedText(null);
+  };
+
+  const handleDelete = () => {
+    if (selectedText) {
+      selectedText.destroy();
+      setSelectedText(null);
+      setShowDeleteButton(false);
+      setText({ value: '', x: 0, y: 0 });
+      stageRef.current.batchDraw();
+    } else if (selectedSticker) {
+      selectedSticker.destroy();
+      setSelectedSticker(null);
+      setShowDeleteButton(false);
+      setStickerImage(null);
+      setStickerScale({ scaleX: 1, scaleY: 1, rotation: 0 });
+      setIsStickerSized(false);
+      fileStickerRef.current.value = '';
+      stageRef.current.batchDraw();
+    }
+  };
+
   const handleReset = () => {
     // Сбросить состояния
     fileInputRef.current.value = '';
     setBackgroundImage(null);
+    //
     setText({ value: '', x: 0, y: 0 });
     setSelectedText(null);
     setShowDeleteButton(false);
     setTextScale({ scaleX: 1, scaleY: 1 });
     setIsTextSized(false);
+    //
+    fileStickerRef.current.value = '';
+    setStickerImage(null);
+    setSelectedSticker(null);
+    setStickerScale({ scaleX: 1, scaleY: 1, rotation: 0 });
+    setIsStickerSized(false);
   };
 
   return (
@@ -103,6 +175,7 @@ const LeftSide = () => {
       </div>
       <div className={stl.canvasBlock}>
         <Stage
+          cornerRadius={50}
           ref={stageRef}
           className={stl.canvas}
           width={356}
@@ -111,10 +184,14 @@ const LeftSide = () => {
             // Проверяем, что клик не был на самом текстовом компоненте или трансформере
             if (
               e.target.className !== 'Text' &&
-              e.target.className !== 'Transformer'
+              e.target.className !== 'Transformer' &&
+              e.target !== stickerRef.current
             ) {
+              setSelectedSticker(null);
+              setIsTextSized(false);
               setSelectedText(null);
               setShowDeleteButton(false);
+              setSelectedType(null);
             }
           }}
         >
@@ -138,7 +215,40 @@ const LeftSide = () => {
                 y={57}
                 width={278}
                 height={303}
-                cornerRadius={[0, 0, 0, 9]}
+              />
+            )}
+            {stickerImage && (
+              <Image
+                ref={stickerRef}
+                x={120} // Установите координату x для стикера
+                y={130} // Установите координату y для стикера
+                width={100} // Установите ширину стикера
+                height={100} // Установите высоту стикера
+                draggable
+                scaleX={stickerScale.scaleX}
+                scaleY={stickerScale.scaleY}
+                onDragEnd={(e) => {
+                  setStickerImage({
+                    ...stickerImage,
+                    x: e.target.x(),
+                    y: e.target.y(),
+                  });
+                }}
+                onClick={handleStickerClick}
+              />
+            )}
+            {selectedSticker && (
+              <Transformer
+                ref={transformerRef}
+                node={selectedSticker}
+                boundBoxFunc={(oldBox, newBox) => {
+                  if (newBox.width < 20 || newBox.height < 20) {
+                    return oldBox;
+                  }
+                  return newBox;
+                }}
+                onTransformEnd={handleStickerTransformEnd}
+                visible={!isStickerSized}
               />
             )}
             {text.value && (
@@ -182,25 +292,50 @@ const LeftSide = () => {
           {showDeleteButton && (
             <Layer>
               <Circle
-                x={text.x - 17}
-                y={text.y - 25}
+                x={
+                  selectedType === 'text'
+                    ? text.x - 17
+                    : selectedType === 'sticker'
+                      ? selectedSticker.x() - 17
+                      : 0
+                }
+                y={
+                  selectedType === 'text'
+                    ? text.y - 25
+                    : selectedType === 'sticker'
+                      ? selectedSticker.y() - 25
+                      : 0
+                }
+                r
                 radius={15}
                 stroke="black"
                 strokeWidth={3}
                 fill="red"
-                onClick={handleDeleteText}
-                visible={!isTextSized}
+                onClick={handleDelete}
+                visible={!isStickerSized || !isTextSized}
               />
               <Text
                 text="X"
-                x={text.x - 23}
-                y={text.y - 31}
+                x={
+                  selectedType === 'text'
+                    ? text.x - 23
+                    : selectedType === 'sticker'
+                      ? selectedSticker.x() - 23
+                      : 0
+                }
+                y={
+                  selectedType === 'text'
+                    ? text.y - 31
+                    : selectedType === 'sticker'
+                      ? selectedSticker.y() - 31
+                      : 0
+                }
                 fontSize={18}
                 fill="#ffffff"
                 align="center"
                 verticalAlign="middle"
-                onClick={handleDeleteText}
-                visible={!isTextSized}
+                onClick={handleDelete}
+                visible={!isStickerSized || !isTextSized}
               />
             </Layer>
           )}
@@ -208,7 +343,16 @@ const LeftSide = () => {
         <img src="/Images/logo/shadow-picture.png" className={stl.shadow}></img>
       </div>
       <div className={stl.btnList}>
-        <Button className={stl.custom}>UPLOAD STICKER</Button>
+        <Button className={stl.custom} onClick={handleStickerUpload}>
+          UPLOAD STICKER
+        </Button>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleStickerChange}
+          ref={fileStickerRef}
+          style={{ display: 'none' }}
+        />
         <Button className={stl.custom} onClick={handleBackgroundUpload}>
           UPLOAD BACKGROUND
         </Button>
